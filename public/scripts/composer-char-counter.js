2783938e8d42c4130ff3962b488b4112fabe6592
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-18 21:50:11 -0500

    added character count functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-  console.log("ready to rock and roll");
+  // Updates the count to reflect the number of characters in the textbox
+  // If over the max amount, count is red and negative
+  $("textarea").keydown(function () {
+    let maxChar = 140;
+    let charCount = $(this).val().replace(/ /g, '').length;
+    if (charCount <= maxChar) {
+      maxChar = maxChar - charCount;
+      return $(this).siblings("span").text(maxChar);
+
+    } else {
+      charCount = maxChar - charCount;
+      return $(this).siblings("span").text(charCount);
+    }
+  });
+
 });
