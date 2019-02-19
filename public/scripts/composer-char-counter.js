943ddfa1d810df50a81866be1e1e0d175a6c095c
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-18 21:55:59 -0500

    Added negative character count class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-  // Updates the count to reflect the number of characters in the textbox
-  // If over the max amount, count is red and negative
+
+  // Updates the character count to reflect the number of characters in the textbox
   $("textarea").keydown(function () {
     let maxChar = 140;
     let charCount = $(this).val().replace(/ /g, '').length;
     if (charCount <= maxChar) {
       maxChar = maxChar - charCount;
       return $(this).siblings("span").text(maxChar);
-
+    // If over the max amount, character count is red and negative
     } else {
       charCount = maxChar - charCount;
-      return $(this).siblings("span").text(charCount);
+      return $(this).siblings("span").text(charCount).addClass('overcount');
     }
+
   });
 
 });
