3b4b64a97b708e25fce2ec46f7886bac3af3c9b6
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-02-20 14:10:59 -0500

    Removed hardcoded database, added function to load tweets from /tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,55 +2,6 @@
 
 $(document).ready(function () {
 
-  // Temporary hardcoded Tweet Database
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
   // Takes in an array of tweet objects and appends each one to the #tweets - container
   function renderTweets(tweets) {
 
@@ -97,14 +48,27 @@ $(document).ready(function () {
     return $tweet;
   }
 
-  // Render the tweet data from the Tweet Databse, creating a new Tweet article for each Tweet
-  renderTweets(data);
+  // Makes a request to Tweet page and receives the array of tweets as JSON
+  function loadTweets() {
 
-  // Fetches newly created tweets from the /tweets page
-  function loadTweets(tweet) {
+    $(function () {
+      $.ajax({
+        type: "GET",
+        url: ('/tweets'),
+        success: function(data) {
+          renderTweets(data);
+        },
+        error: function () {
+          console.log('An error occurred.');
+        },
+      });
+    });
+
   }
 
+  // Fetches tweets from /tweets
+  loadTweets();
+
+
+// End of Document Ready
 });
-
-
-
